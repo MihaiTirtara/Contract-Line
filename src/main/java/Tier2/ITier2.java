51f37224a53efcdfdc4e4a4976538b65b6c8880d
unknown <mihai.tirtara@gmail.com>
2018-11-21 01:37:04 +0100

    Tier2 added

--- src/main/java/Tier2/ITier2.java
+++ src/main/java/Tier2/ITier2.java
@@ -1,7 +1,12 @@
 package Tier2;
 
-public interface ITier2 
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface ITier2 extends Remote
 {
+	public void Register(String id, String password,String phoneNumber,String fName,String lName) throws RemoteException;
 	
+	public static final String T2_SERVICE_NAME = "rmi://localhost/T2";
 
 }
